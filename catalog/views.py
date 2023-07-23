d88cf0aab1faaa0179f4aca865389a9a549ba0b0
Helen85236 <79532870251@yandex.ru>
2023-07-23 10:48:34 +0300

    1-4

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
+    # Convert QuerySet to List
+    list_products = list(Product.objects.all())
+    # Print last five records
+    if len(list_products) >= 5:
+        for product in list_products[-5:]:
+            print(product)
+    else:
+        for product in list_products:
+            print(product)
     return render(request, 'index.html')
 
 def contacts(request):
+    data = Contact.objects.all()
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'{name} ({email}): {message}')
-    return render(request, 'contacts.html')
+    return render(request, 'contacts.html',{"contacts": data})
