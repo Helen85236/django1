fd5a0dea967506f9905f0c6605ed2a3126d60cda
Helen85236 <79532870251@yandex.ru>
2023-08-04 12:11:48 +0300

    ProductDetailView??

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, category_list, product
+from catalog.views import IndexView, ProductDetailView, contacts, category_list # product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
     path('category/', category_list),
-    path('product/<int:id_product>', product),
+    path('product/<int:id_product>', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
 
 from catalog.models import Product, Contact, Category
 
 
-def index(request):
-    # Convert QuerySet to List
-    list_products = list(Product.objects.all())
-    # Print last five records
-    # if len(list_products) >= 5:
-    #     for product in list_products[-5:]:
-    #         print(product)
-    # else:
-    #     for product in list_products:
-    #         print(product)
-    # context = {
-    #     'object_list': list_products,
-    # }
-    return render(request, 'index.html', context)
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-    # Get all products from catalog
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Product.objects.all()
+        return context_data
+
+
+# def index(request):
+#     # Convert QuerySet to List
+#     list_products = list(Product.objects.all())
+#     # Print last five records
+#     # if len(list_products) >= 5:
+#     #     for product in list_products[-5:]:
+#     #         print(product)
+#     # else:
+#     #     for product in list_products:
+#     #         print(product)
+#     # context = {
+#     #     'object_list': list_products,
+#     # }
+#     return render(request, 'index.html', context)
+#
+#     # Get all products from catalog
+
 def category_list(request):
     category = list(Category.objects.all())
 
@@ -28,14 +39,18 @@ def category_list(request):
 
     return render(request, 'category_list.html', context)
 
-def product(request,id_product):
-    product = Product.objects.get(id=id_product)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
 
-    context = {
-        'object': product,
-    }
-
-    return render(request, 'product_detail.html', context)
+# def product(request,id_product):
+#     product = Product.objects.get(id=id_product)
+#
+#     context = {
+#         'object': product,
+#     }
+#
+#     return render(request, 'product_detail.html', context)
 
 
 
